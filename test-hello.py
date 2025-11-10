5aed181e0f70c7925f96da11e958ed56c3a7c992
Mubarek-Amin <aminmubarek@icloud.com>
2025-11-10 22:27:41 +0000

    changed workflow

--- test-hello.py
+++ test-hello.py
@@ -4,3 +4,7 @@ from hello import hello_world
 class testHello(unittest.TestCase):
     def test(self):
         self.assertEqual(hello_world(), "hello world")
+
+
+if __name__ == "__main__":
+    unittest.main()
